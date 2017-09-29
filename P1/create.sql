5dd24e5e133b4baa59153b21a52e8d921f9d1083
Wei Li <weil@do-not-connect.local>
2017-09-28 21:16:30 -0500

    Add load.txt file to load .dat file and replace the Null value. And add some fix to create file

--- P1/create.sql
+++ P1/create.sql
@@ -1,47 +1,47 @@
-drop table if exists Item;
-drop table if exists User;
+drop table if exists Items;
+drop table if exists Users;
 drop table if exists Bids;
 drop table if exists Category;
 
---the correct create order should be user --> Item --> Bids --> Category since each of them are depended on each other
+--the correct create order should be users --> Items --> Bids --> Category since each of them are depended on each other
 
-create table User(
-	UserID		bigint,
-	Country		string,
-	Location	string,
-	Rating		int,
+create table Users(
+	UserID		FLOAT,
+	Country		VARCHAR,
+	Location	VARCHAR,
+	Rating		INT,
 	PRIMARY KEY (UserID)
 ) 
 
-create table Item(
-	ItemID		bigint,
-	Name		string NOT NULL,
-	Buy_Price	double,
-	First_Bid	double NOT NULL,
-	Number_of_Bids	int NOT NULL,
-	Started		date NOT NULL, 
-	Ends		date NOT NULL,
-	Currently	double NOT NULL,
-	Seller_ID	bigint NOT NULL,
-	Description	string NOT NULL,
+create table Items(
+	ItemID		FLOAT,
+	Name		VARCHAR NOT NULL,
+	Buy_Price	DOUBLE,
+	First_Bid	DOUBLE NOT NULL,
+	Number_of_Bids	INT NOT NULL,
+	Started		DATE NOT NULL, 
+	Ends		DATE NOT NULL,
+	Currently	DOUBLE NOT NULL,
+	Seller_ID	FLOAT NOT NULL,
+	Description	VARCHAR NOT NULL,
 	PRIMARY KEY (ItemId),
-	FOREIGN KEY (Seller_ID) REFERENCES User(UserID)
+	FOREIGN KEY (Seller_ID) REFERENCES Users(UserID)
 )
 
 create table Bids(
-	UserID		bigint,
-	Time		date NOT NULL,
-	Amount		double NOT NULL,
-	ItemID		bigint NOT NULL,
+	UserID		FLOAT,
+	Time		DATE NOT NULL,
+	Amount		VARCHAR NOT NULL,
+	ItemID		FLOAT NOT NULL,
 	PRIMARY KEY (UserID, Time, Amount, ItemID), --maybe can be reduce to 3
-	FOREIGN KEY (UserID) REFERENCES User(UserID),
-	FOREIGN KEY (ItemID) REFERENCES Item(ItemID)
+	FOREIGN KEY (UserID) REFERENCES Users(UserID),
+	FOREIGN KEY (ItemID) REFERENCES Items(ItemID)
 )
 
 create table Category(
-	Name		string,
-	ItemID		bigint,
+	Name		VARCHAR,
+	ItemID		FLOAT,
 	PRIMARY KEY (Name, ItemID),
-	FOREIGN KEY (ItemID) REFERENCES Item(ItemID)
+	FOREIGN KEY (ItemID) REFERENCES Items(ItemID)
 )
 
